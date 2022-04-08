4b8df80854ea58adc8a39da678d436aa4d4af399
AmandaMariaFS <amandinhaferre@hotmail.com>
2022-04-08 19:38:24 -0300

    Incremento da funcionalidade de informar a faixa de IMC do usuário

--- Program.cs
+++ Program.cs
@@ -12,3 +12,32 @@ double imc = peso / (altura * altura);
 
 Console.WriteLine("{0}, sua altura é {1} e seu peso {2}", nome, altura, peso);
 Console.WriteLine("Seu IMC é {0}", imc);
+
+if (imc > 40)
+{
+    Console.WriteLine("Você está na faixa de Obesidade III");
+}
+else if (imc > 35)
+{
+    Console.WriteLine("Você está na faixa de Obesidade II");
+}
+else if (imc > 30)
+{
+    Console.WriteLine("Você está na faixa de Obesidade I");
+}
+else if (imc > 25)
+{
+    Console.WriteLine("Você está acima do peso");
+}
+else if (imc > 18.5)
+{
+    Console.WriteLine("Você está no peso normal");
+}
+else if (imc > 17)
+{
+    Console.WriteLine("Você está abaixo do peso");
+}
+else
+{
+    Console.WriteLine("Você está muito abaixo do peso");
+}
